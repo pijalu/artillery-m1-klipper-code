fb771238aefff99747f85a36f1007ac410dd48fc
Pierre Poissinger <pierre.poissinger@gmail.com>
2026-03-29 11:31:27 +0200

    Replace proprietary hall

--- klippy/extras/hall_fila_switch.py
+++ klippy/extras/hall_fila_switch.py
@@ -1,4 +1,9 @@
-from . import hall_filament_switch_sensor
+# Overrules Artillery M1 Pro proprietary BS
+#
+# Copyright (C) 2026  Pierre Poissinger <pierre.poissinger@gmail.com>
+#
+# This file may be distributed under the terms of the GNU GPLv3 license.
+from . import hall_filament_width_sensor
 
 def load_config(config):
-    return hall_filament_switch_sensor.HallFilamentSwitchSensor(config)
+    return hall_filament_width_sensor.HallFilamentWidthSensor(config)
